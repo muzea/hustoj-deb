fd687c82bca9535533a1ed02998f26ecbaef971d
MuYu <mr.muzea@gmail.com>
2019-06-13 14:24:26 +0800

    fix: path error

--- build.sh
+++ build.sh
@@ -11,8 +11,9 @@ set -ex \
 && cd /root \
 && git clone -b $SCRIPT_BRANCH https://github.com/muzea/hustoj-deb.git \
 && mkdir -p $BUILDDIR \
-&& cp -R /root/hustoj-deb/common $BUILDDIR \
-&& cp -RT /root/hustoj-deb/${DISTRIBUTION} $BUILDDIR \
+&& cp -R /root/hustoj-deb/common/* $BUILDDIR \
+&& cp -R /root/hustoj-deb/${DISTRIBUTION}/* $BUILDDIR \
 && mv /hustoj/trunk/* $BUILDDIR \
 && cd $BUILDDIR && dpkg-buildpackage \
 && mv /root/hustoj_${VERSION}_amd64.deb /tmp/deb/hustoj_${DISTRIBUTION}_${VERSION}-${TRAVIS_TAG}_amd64.deb
+
